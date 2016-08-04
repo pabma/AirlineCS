afee44a1d46abca5f4df6024eb3da7196f09daa5
Shrihari Bhat <shrihariabhat@gmail.com>
2016-08-04 12:25:22 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,39 @@
+
+public class User {
+	String username;
+	String password;
+	int age;
+	String address;
+	String gender;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+}
